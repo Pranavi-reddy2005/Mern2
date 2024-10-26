f76f28c3580088221b30a581c37777ae2183c45b
Pranavi-reddy2005 <ammireddypranavi35@gmail.com>
2024-10-27 00:56:08 +0530

    D6

---
+++ React/route-project/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const Home = () => {
+  return (
+    <>
+       <p>This is Pranavi Reddy</p>
+    
+    </>
+  )
+}
+
+export default Home
