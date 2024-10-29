51705534c325e0f6eae142c3c1e471a383121a2f
Pranavi-reddy2005 <ammireddypranavi35@gmail.com>
2024-10-29 08:57:30 +0530

    D7 : Project

--- Project/store-client/src/api/api.js
+++ Project/store-client/src/api/api.js
@@ -1,8 +1,11 @@
-import axios from 'axios' 
 
-const API = 
-
- getProducts = () => axios.get(API)
+import axios from 'axios'
 
 
-export{getProducts}
+const API = 'https://671b70ef2c842d92c37fec0c.mockapi.io/prodcutsapp/products'
+
+
+const getProducts = () => axios.get(API)
+
+
+export { getProducts }
--- Project/store-client/src/pages/Home.jsx
+++ Project/store-client/src/pages/Home.jsx
@@ -1,15 +1,17 @@
+import HomeCards from "../components/HomeCards"
 import HomeCarousel from "../components/HomeCarousel"
-
+import SocialCard from "../components/SocialCard"
 
 const Home = () => {
-  return (
-    <>
-      <div className='w-full h-full flex flex-row justify-center items-center gap-8'>
-     <HomeCarousel />
-
-     </div>
-    </>
-  )
+    return (
+        <>
+            <div className="flex flex-col justify-start items-start gap-8 w-screen h-full">
+                <HomeCarousel />
+                <HomeCards />
+                <SocialCard />
+            </div>
+        </>
+    )
 }
 
 export default Home
