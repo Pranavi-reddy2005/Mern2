fd763f29989309d4ab44a499752d3cbbc196784d
Pranavi-reddy2005 <ammireddypranavi35@gmail.com>
2024-11-10 16:38:14 +0530

    D-13

--- Express/server-mongoose/config/auth.js
+++ Express/server-mongoose/config/auth.js
@@ -1,14 +1,20 @@
+const jwt = require('jsonwebtoken') 
 const validateToken = (req, res, next) => {
     const secretkey = '1234'
     const token = req.headers.authorization
-    console.log(token)
+    // console.log(token)
     if (!token) {
-        return res.status(401).json({ message: "Invalid token" })
+        return res.status(401).json({ message: "Invalid Request" })
     }
     try {
+        const validate =  jwt.verify(token, secretkey)
+        const exp = validate.exp
+        if(exp < (Date.now() / 1000) ){
+            return res.status(500).json({message: "Token expired"})
+        }
         next()
     } catch (error) {
-
+        return res.status(500).json({ message: "Invalid token" })
     }
 }
 
